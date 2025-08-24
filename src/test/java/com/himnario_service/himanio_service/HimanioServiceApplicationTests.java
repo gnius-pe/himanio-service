90b68b011cc836dd3fd6fc1d8a4af7e3baf6c490
igorrc <igor.ramos.cruzado.w@gmail.com>
2025-08-23 23:05:59 -0500

    homnario service

---
+++ src/test/java/com/himnario_service/himanio_service/HimanioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himnario_service.himanio_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HimanioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
