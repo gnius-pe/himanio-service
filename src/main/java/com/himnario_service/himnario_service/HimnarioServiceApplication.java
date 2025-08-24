e242a86bdcc5358740ecfaf67ed9742dfc93229f
igorrc <igor.ramos.cruzado.w@gmail.com>
2025-08-23 23:09:06 -0500

    new restructure project

---
+++ src/main/java/com/himnario_service/himnario_service/HimnarioServiceApplication.java
@@ -0,0 +1,13 @@
+package com.himnario_service.himnario_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HimnarioServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HimnarioServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/himnario_service/himnario_service/HimnarioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himnario_service.himnario_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HimnarioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
